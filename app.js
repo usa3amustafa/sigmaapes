a9498e7c02f592322a4d02a4e0b4943de0d05946
usa3amustafa <usa3a3ustafa@outlook.com>
2022-03-03 10:55:43 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const slides = document.querySelectorAll('.step')
+const btnRight = document.querySelector('.right-slide-btn')
+const btnLeft = document.querySelector('.left-slide-btn')
+
+let currSlide = 0
+const maxSlide = slides.length
+
+function goToSlide(slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${42 * (i - slide)}rem)`)
+  )
+}
+
+goToSlide(currSlide)
+
+btnRight.addEventListener('click', function () {
+  if (currSlide === maxSlide - 1) {
+    currSlide = 0
+  } else {
+    currSlide++
+  }
+
+  goToSlide(currSlide)
+})
+
+btnLeft.addEventListener('click', function () {
+  if (currSlide === 0) {
+    currSlide = maxSlide - 1
+  } else {
+    currSlide--
+  }
+
+  goToSlide(currSlide)
+})
