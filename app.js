b82ec6dc7602586606a96e201be537f73b67063c
usa3amustafa <usa3a3ustafa@outlook.com>
2022-03-04 16:17:37 +0500

    navbar

--- app.js
+++ app.js
@@ -32,3 +32,42 @@ btnLeft.addEventListener('click', function () {
 
   goToSlide(currSlide)
 })
+
+// navbar
+
+const openMenuBtn = document.querySelector('.hamburger')
+const closeMenuBtn = document.querySelector('.close-icon')
+const navbar = document.querySelector('.nav-links')
+const navLinks = document.querySelectorAll('.nav-link')
+
+const showNavbar = function () {
+  navbar.classList.add('show-nav')
+  openMenuBtn.classList.add('hidden')
+}
+
+const removeNavbar = function () {
+  navbar.classList.remove('show-nav')
+  openMenuBtn.classList.remove('hidden')
+}
+
+openMenuBtn.addEventListener('click', showNavbar)
+closeMenuBtn.addEventListener('click', removeNavbar)
+
+navLinks.forEach(navLink => navLink.addEventListener('click', removeNavbar))
+
+// const hamburger = document.querySelector('.hamburger')
+// const closeIcon = document.querySelector('.close-icon')
+
+// const nav = document.querySelector('.nav-links')
+
+// hamburger.addEventListener('click', function () {
+//   hamburger.style.display = 'none'
+//   closeIcon.style.display = 'block'
+//   nav.classList.add('show-nav')
+// })
+
+// closeIcon.addEventListener('click', function () {
+//   closeIcon.style.display = 'none'
+//   hamburger.style.display = 'block'
+//   nav.classList.remove('show-nav')
+// })
